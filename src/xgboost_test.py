4a583b383f33a7bef41df7da8613c590dd74a959
GaetanC <chambres.gaetan@gmail.com>
2018-04-18 17:17:35 +0200

    Adding possibility to train on 2 datasets and test on the second

--- src/xgboost_test.py
+++ src/xgboost_test.py
@@ -9,14 +9,25 @@ from sklearn.metrics import precision_score
 #####     Open Files     #####
 ##############################
 
+DS1_features = pd.read_csv('./data/csv/mfccs_csv/DS1features.csv', header=None)
+DS1_fileinfo = pd.read_csv('./data/csv/initial_csv/DS1global.csv', header=None)
+
+DS2_features = pd.read_csv('./data/csv/mfccs_csv/DS2features.csv', header=None)
+DS2_fileinfo = pd.read_csv('./data/csv/initial_csv/DS2global.csv', header=None)
+
+DS3_features = pd.read_csv('./data/csv/mfccs_csv/DS3features.csv', header=None)
+DS3_fileinfo = pd.read_csv('./data/csv/initial_csv/DS3global.csv', header=None)
+
 #####   train   #####
-train_mfcc = pd.read_csv('./data/csv/mfccs_csv/DS1features.csv', header=1)
-train_fileinfo = pd.read_csv('./data/csv/initial_csv/DS1global.csv', header=1)
+train_mfcc = DS1_features
+train_fileinfo = DS1_fileinfo
+
+train_mfcc_2 = DS2_features
+train_fileinfo_2 = DS2_fileinfo
 
 #####   test    #####
-test_mfcc = pd.read_csv('./data/csv/mfccs_csv/DS3features.csv', header=1)
-test_fileinfo = pd.read_csv('./data/csv/initial_csv/DS3global.csv', header=1)
-
+test_mfcc = DS3_features
+test_fileinfo = DS3_fileinfo
 
 ##############################
 #  Naming cols in csv files  #
@@ -28,6 +39,7 @@ tmp_info = tmp + "crackles,wheezles"
 cols_labels_info = tmp_info.split(",")
 #print(cols_labels_info)
 train_fileinfo.columns = cols_labels_info
+train_fileinfo_2.columns = cols_labels_info
 test_fileinfo.columns = cols_labels_info
 # DEBUG: prints
 #train_fileinfo.info()
@@ -48,6 +60,7 @@ tmp_mfcc = tmp_mfcc[:-1]
 cols_labels_mfcc = tmp_mfcc.split(",")
 #print cols_labels_mfcc
 train_mfcc.columns = cols_labels_mfcc
+train_mfcc_2.columns = cols_labels_mfcc
 test_mfcc.columns = cols_labels_mfcc
 # DEBUG: prints
 #train_mfcc.info()
@@ -64,22 +77,41 @@ test_mfcc.columns = cols_labels_mfcc
 ##############################
 
 #####   train   #####
+print("TRAIN inputs")
 dataset_train_mfcc = train_mfcc[[col for col in train_mfcc.columns if col!='filename' and col!='startTime' and col!= 'endTime']]
-# print(dataset_train_mfcc.shape)
+dataset_train_mfcc_2 = train_mfcc_2[[col for col in train_mfcc_2.columns if col!='filename' and col!='startTime' and col!= 'endTime']]
+combined_train_mfcc = pd.concat([dataset_train_mfcc, dataset_train_mfcc_2],axis=0,ignore_index=True) # Stacks them vertically
+print("shape of data input 1:",dataset_train_mfcc.shape)
+print("shape of data input 2:",dataset_train_mfcc_2.shape)
+print("shape of combined data input:",combined_train_mfcc.shape)
+print()
 
 crackles_train = train_fileinfo[[col for col in train_fileinfo.columns if col!='filename' and col!='startTime' and col!= 'endTime' and col!='wheezles']]
+crackles_train_2 = train_fileinfo_2[[col for col in train_fileinfo_2.columns if col!='filename' and col!='startTime' and col!= 'endTime' and col!='wheezles']]
+combined_crackles_train = pd.concat([crackles_train, crackles_train_2],axis=0,ignore_index=True)
+print("shape of crackles results 1:",crackles_train.shape)
+print("shape of crackles results 2:",crackles_train_2.shape)
+print("shape of combined crackles results:",combined_crackles_train.shape)
+print()
+
 wheezles_train = train_fileinfo[[col for col in train_fileinfo.columns if col!='filename' and col!='startTime' and col!= 'endTime' and col!='crackles']]
-# print(crackles_train.shape)
-# print(wheezles_train.shape)
+wheezles_train_2 = train_fileinfo_2[[col for col in train_fileinfo_2.columns if col!='filename' and col!='startTime' and col!= 'endTime' and col!='crackles']]
+combined_wheezles_train = pd.concat([wheezles_train, wheezles_train_2],axis=0,ignore_index=True)
+print("shape of wheezles results 1:",wheezles_train.shape)
+print("shape of wheezles results 2:",wheezles_train_2.shape)
+print("shape of combined wheezles results:",combined_wheezles_train.shape)
+print()
 
 #####   test    #####
+print("TEST inputs")
 dataset_test_mfcc = test_mfcc[[col for col in test_mfcc.columns if col!='filename' and col!='startTime' and col!= 'endTime']]
-# print(dataset_test_mfcc.shape)
+print("shape of data input",dataset_test_mfcc.shape)
 
 crackles_test = test_fileinfo[[col for col in test_fileinfo.columns if col!='filename' and col!='startTime' and col!= 'endTime' and col!='wheezles']]
 wheezles_test = test_fileinfo[[col for col in test_fileinfo.columns if col!='filename' and col!='startTime' and col!= 'endTime' and col!='crackles']]
-# print(crackles_test.shape)
-# print(wheezles_test.shape)
+print("shape of crackles results",crackles_test.shape)
+print("shape of wheezles results",wheezles_test.shape)
+print()
 
 ##################################################
 #####          Training and predictions      #####
@@ -99,7 +131,8 @@ num_round = 20  # the number of training iterations
 #--------------------------------
 #####   Working on crackles #####
 #--------------------------------
-crackles_matrix_train = xgb.DMatrix(dataset_train_mfcc, label=crackles_train)
+crackles_matrix_train = xgb.DMatrix(combined_train_mfcc, label=combined_crackles_train)
+#crackles_matrix_train = xgb.DMatrix(dataset_train_mfcc, label=crackles_train)
 crackles_matrix_test = xgb.DMatrix(dataset_test_mfcc, label=crackles_test)
 
 # training and testing - numpy matrices
@@ -113,7 +146,8 @@ print("Precision of prediction for crackles: " + str(precision_score(crackles_te
 #--------------------------------
 #####   Working on wheezles #####
 #--------------------------------
-wheezles_matrix_train = xgb.DMatrix(dataset_train_mfcc, label=wheezles_train)
+wheezles_matrix_train = xgb.DMatrix(combined_train_mfcc, label=combined_wheezles_train)
+#wheezles_matrix_train = xgb.DMatrix(dataset_train_mfcc, label=wheezles_train)
 wheezles_matrix_test = xgb.DMatrix(dataset_test_mfcc, label=wheezles_test)
 
 # training and testing - numpy matrices
